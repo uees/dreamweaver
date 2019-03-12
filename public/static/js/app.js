ab021756f0f46f02d617825cf513b9c150c438f5
Wan <wnh3yang@gmail.com>
2019-03-12 21:06:16 +0800

    修复划线bug

--- public/static/js/app.js
+++ public/static/js/app.js
@@ -1,36 +1,10 @@
 'use strict';
 
-const createPoint = function (container, x, y) {
-    const node = document.createElement('div');
-    node.className = 'line';
-    node.style.top = y + 'px';
-    node.style.left = x + 'px';
-    container.appendChild(node);
-};
-
-const line = function (startX, startY, endX, endY, container) {
-    if (startX === endX) {
-        if (startY > endY) {
-            let tempY = startY;
-            startY = endY;
-            endY = tempY;
-        }
-        for (let k = startY; k < endY; k++) {
-            createPoint(container, startX, k);
-        }
-    }
-    // y = ax + b
-    let a = (startY - endY) / (startX - endX);
-    let b = startY - ((startY - endY) / (startX - endX)) * startX;
-
-    if (startX > endX) {
-        let tempX = endX;
-        endX = startX;
-        startX = tempX;
-    }
-    for (let i = startX; i <= endX; i++) {
-        createPoint(container, i, a * i + b);
-    }
+const drawLine = function(context, color, start, end) {
+    context.strokeStyle = color;
+    context.moveTo(start.left, start.top);
+    context.lineTo(end.left, end.top);
+    context.stroke();
 };
 
 $(function () {
